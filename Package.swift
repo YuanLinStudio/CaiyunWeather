955717da5f6eaa64776b0de68a9130245ec88bf8
Lin Yuan <yuanlin0921@gmail.com>
2021-06-17 15:11:29 -0700

    General

--- Package.swift
+++ Package.swift
@@ -30,7 +30,6 @@ let package = Package(
             resources: [
                 .process("Memos.md"),
                 .process("Resources/Weather.json"),
-                .process("README-zh.md")
             ]
         ),
         .testTarget(
